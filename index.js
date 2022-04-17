1e776ce53ebac43f2c51ea8977384cb16ee11bcb
Zisan Uzum <63046344+Zisan-uzum@users.noreply.github.com>
2022-04-17 22:23:20 +0300

    Update index.js
    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+// Code your solutions in this file
+function writeCards(array, event) {
+  let res = [];
+  for (let i = 0; i < array.length; i++) {
+    res.push(`Thank you, ${array[i]}, for the wonderful ${event} gift!`);
+  }
+  return res;
+}
+function countDown(number) {
+  for (let i = number; i >= 0; i--) {
+    console.log(i);
+  }
+}
